5633c6ecf0426971ac504217eb246a9cb056ac2b
Yunhee Choi <yoonh12288@gmail.com>
2025-07-12 19:06:13 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>    
+using namespace std;
+
+int main() {
+    long long a, b, v;
+    cin >> a >> b >> v;
+    
+    
+    
+    
+        cout<<((v - b - 1)/(a-b))+1;
+   
+}
