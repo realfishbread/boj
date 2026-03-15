801b6698238613da2ee945f68e099587773daca9
Yunhee Choi <yoonh12288@gmail.com>
2026-03-15 23:36:40 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int n,m;
+int seq[9];
+
+
+void dfs(int depth, int start){
+    if(depth==m){
+        for(int i=0; i<m; i++){
+            cout<<seq[i]<<(i+1 ==m? "\n":" ");
+        }
+        return;
+    }
+    
+    for(int x=start; x<=n; x++){
+        seq[depth]=x;
+        dfs(depth+1,x);
+    }
+}
+
+int main()
+{
+    cin>>n>>m;
+    dfs(0,1);
+    return 0;
+}
